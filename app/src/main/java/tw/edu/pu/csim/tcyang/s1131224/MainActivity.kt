368f3a4800297491186dfb8f0fe6ca74cc75a829
zii10 <lekent1992@gmail.com>
2025-12-01 14:49:24 +0800

    v1_周子洋

--- app/src/main/java/tw/edu/pu/csim/tcyang/s1131224/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/s1131224/MainActivity.kt
@@ -1,6 +1,9 @@
 package tw.edu.pu.csim.tcyang.s1131224
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -14,9 +17,14 @@ import androidx.compose.ui.tooling.preview.Preview
 import tw.edu.pu.csim.tcyang.s1131224.ui.theme.S1131224Theme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
         enableEdgeToEdge()
+
         setContent {
             S1131224Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -27,15 +35,28 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        // 👉 隱藏系統列（狀態列 + 導航列）
+        hideSystemBars()
+    }
+
+    private fun hideSystemBars() {
+        val controller = window.insetsController ?: return
+
+        // 隱藏狀態列 & 導航列
+        controller.hide(
+            WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars()
+        )
+
+        // 沉浸模式：滑動可暫時喚出系統列
+        controller.systemBarsBehavior =
+            WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
     }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Text(text = "Hello $name!", modifier = modifier)
 }
 
 @Preview(showBackground = true)
